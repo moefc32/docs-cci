a6a261b345f049d876481acca4ac01b4ed129f3c
Faizal Chan. <moefc32@gmail.com>
2025-09-11 10:07:06 +0700

    Updates

--- _app/immutable/entry/start.BBYW9_0U.js
+++
@@ -1,1 +0,0 @@
-import{l as o,a as r}from"../chunks/Do_LjpBJ.js";export{o as load_css,r as start};
--- _app/immutable/nodes/2.CAp8b9ob.js
+++
@@ -1,8 +0,0 @@
-import{f as d,h as u,a as s,s as f}from"../chunks/B_sOT1Xe.js";import{i as h}from"../chunks/BBaCRE_r.js";import{p as g,t as n,a as v,u as t,$ as x,c as o,r,n as y}from"../chunks/C_jl_BMB.js";import{s as w}from"../chunks/B_riCdbT.js";var b=d('<meta property="og:title"/>'),C=d(`<div class="flex flex-1 flex-col gap-3"><h1 class="my-3 pb-3 text-5xl border-b-[1px] border-gray-700"> </h1> <p>Simpledocs is a minimalistic and efficient documentation site designed
-        for designers, developers, and teams who need a quick and organized way
-        to present their documentation. It offers a clean and responsive layout
-        with essential features like a navigation sidebar and search
-        functionality. Built with simplicity in mind, this project ensures that
-        users can focus on writing content without dealing with complex
-        configurations. Its lightweight nature makes it easy to deploy on the
-        GitHub Pages.</p></div>`);function G(l,p){g(p,!1),h();var e=C();u(m=>{var i=b();n(()=>{x.title=t(()=>"CCI Brand Guidelines"),w(i,"content",t(()=>"CCI Brand Guidelines"))}),s(m,i)});var a=o(e),c=o(a,!0);r(a),y(2),r(e),n(()=>f(c,t(()=>"CCI Brand Guidelines"))),s(l,e),v()}export{G as component};
